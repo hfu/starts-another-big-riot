f86950760bcc442b5d0ab63c95238386f4f4c51a
Hidenori FUJIMURA <hfu@users.noreply.github.com>
2016-07-15 21:55:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var map = L.map("map").setView([35.362222, 138.731389], 5);
-L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png", {
-  attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>淡色地図 (地理院タイル)</a>"
+L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
+  attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>標準地図 (地理院タイル)</a>"
 }).addTo(map);
 
 fetch("https://cyberjapandata.gsi.go.jp/xyz/20160414kumamoto_0416uav/2/3/1.geojson")
